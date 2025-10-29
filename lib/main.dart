32b8983a1951fc089f50e9b8c0e4d546177fdf8e
M. Fazri Nizar <anon6372098.id@gmail.com>
2025-10-30 03:18:45 +0700

    feat: add assets and main along with old module 1 page

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:wisataku/view/wira_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarIconBrightness: Brightness.light,
+    statusBarBrightness: Brightness.light,
+  ));
+  runApp(const WiraApp());
+}
+
+class WiraApp extends StatelessWidget {
+  const WiraApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wiraku App',
+      theme: ThemeData(
+        fontFamily: 'Poppy',
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const WiraView(),
+    );
+  }
+}
